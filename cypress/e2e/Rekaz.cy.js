0672fbf6b2d81a18fb588892b37e575fd1af1f7b
ahmed3QS <esee22222@gmail.com>
2025-02-27 18:37:13 +0300

    Updated Rekaz.cy.js with latest changes

--- cypress/e2e/Rekaz.cy.js
+++ cypress/e2e/Rekaz.cy.js
@@ -1,19 +1,23 @@
 it('Email is wrong', () => {
   cy.visit('https://platform.rekaz.dev/');
 
-  // Login
+  
   cy.get('#LoginInput_UserNameOrEmailAddress').should('be.visible').type('ahmed@gmail.com');
+  cy.wait(2000);
   cy.get('#LoginInput_Password').type('Aa123456');
+  cy.wait(2000);
   cy.get('.btn-dark').click();
-  cy.wait(3000)
+  cy.wait(3000);
 
 })
 it('Password is wrong', () => {
   cy.visit('https://platform.rekaz.dev/');
 
-  // Login
+  
   cy.get('#LoginInput_UserNameOrEmailAddress').should('be.visible').type('esee22222@gmail.com');
+  cy.wait(2000);
   cy.get('#LoginInput_Password').type('Aa1234');
+  cy.wait(2000);
   cy.get('.btn-dark').click();
   cy.wait(3000)
 
@@ -22,7 +26,7 @@ it('No input', () => {
   cy.visit('https://platform.rekaz.dev/');
 
   
- 
+  cy.wait(2000);
   cy.get('.btn-dark').click();
   cy.wait(3000)
 
@@ -32,11 +36,12 @@ it('No input', () => {
  it('Both entries are correct', () => {
     cy.visit('https://platform.rekaz.dev/');
 
-    // Login
+    
     cy.get('#LoginInput_UserNameOrEmailAddress').should('be.visible').type('esee22222@gmail.com');
+    cy.wait(2000);
     cy.get('#LoginInput_Password').type('Aa123456');
+    cy.wait(2000);
     cy.get('.btn-dark').click();
-
     cy.wait(4000);
 
 
@@ -61,6 +66,7 @@ it('No input', () => {
     cy.wait(1000);
 
     cy.get(':nth-child(8) > .menu-link').click();
+    cy.wait(2000);
     cy.get('#AbpContentToolbar > .align-items-center > .btn').click();
 
     cy.wait(1000);
@@ -96,10 +102,15 @@ it('No input', () => {
 
     cy.wait(1000);
 
-    cy.get('#CouponModel_Code').type('TestRekaz');
+    cy.get('#CouponModel_Code').type('TestRekaz4');
+    cy.wait(2000);
     cy.get('.mb-5 > .select2 > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').should('be.visible').click();
+    cy.wait(2000);
     cy.get('[role="group"] > .select2-results__options > .select2-results__option').should('be.visible').click();
+    cy.wait(2000);
     cy.get('.row > :nth-child(1) > .btn').click();
+    
+    cy.wait(2000);
     cy.get('#discount_fixed > #CouponModel_DiscountValue').clear().type('ahmed');
     
     cy.wait(1000);
@@ -112,61 +123,6 @@ it('No input', () => {
   })
 
 
-
-  
-
-
-
-
-
- it('Both entries are correct', () => {
-    cy.visit('https://platform.rekaz.dev/');
-
-    // Login
-    cy.get('#LoginInput_UserNameOrEmailAddress').should('be.visible').type('esee22222@gmail.com');
-    cy.get('#LoginInput_Password').type('Aa123456');
-    cy.get('.btn-dark').click();
-
-    cy.wait(1000);
-
-    cy.get(':nth-child(8) > .menu-link').click();
-    cy.get('#AbpContentToolbar > .align-items-center > .btn').click();
-
-    cy.wait(1000);
-
-    cy.get('#CouponModel_Code').type('TestRekaz');
-    cy.get('.mb-5 > .select2 > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').should('be.visible').click();
-    cy.get('[role="group"] > .select2-results__options > .select2-results__option').should('be.visible').click();
-    cy.get('.row > :nth-child(1) > .btn').click();
-    cy.get('#discount_fixed > #CouponModel_DiscountValue').clear().type('200');
-    
-    cy.wait(1000);
-    
-    cy.get('#CouponModel_TotalUsageForAll').type('20');
-    cy.get('#CouponModel_TotalUsagePerCustomer').type('10');
-    cy.get('#CouponModel_MinimumCartAmount').type('30');
-    cy.wait(1000);
-    
-   //     التاريخ
-    cy.get('#form_date_picker').click();
-
-    cy.wait(500); 
-
-    cy.get('.open > .flatpickr-innerContainer > .flatpickr-rContainer > .flatpickr-days > .dayContainer > [aria-label="January 26, 2025"]').click();
-
-    cy.get('#to_date_picker').click();
-    cy.get('[aria-label="February 16, 2025"]').click();
-
-    cy.get('#btn-submit').click();  
-    cy.wait(1000);
-
-
-    cy.wait(4000);
-  })
-
-
-
-  
 
 
 
@@ -183,6 +139,7 @@ it('No input', () => {
     cy.wait(1000);
 
     cy.get(':nth-child(8) > .menu-link').click();
+    cy.wait(2000);
     cy.get('#AbpContentToolbar > .align-items-center > .btn').click();
 
     cy.wait(1000);
@@ -228,6 +185,7 @@ it('No input', () => {
     cy.wait(1000);
 
     cy.get('#CouponModel_Code').type('TestRekaz');
+    cy.wait(2000);
     cy.get('.mb-5 > .select2 > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').should('be.visible').click();
     cy.get('[role="group"] > .select2-results__options > .select2-results__option').should('be.visible').click();
     cy.get('.row > :nth-child(1) > .btn').click();
@@ -273,7 +231,9 @@ it('No input', () => {
 
     cy.wait(1000);
 
-    cy.get('#CouponModel_Code').type('S');
+    cy.get('#CouponModel_Code').type('SS');
+    cy.wait(2000);
+
     cy.get('#btn-submit').click();  
    
 
@@ -300,7 +260,7 @@ it('No input', () => {
     cy.wait(1000);
     cy.get('#CouponModel_IsActive').click();
     cy.wait(3000)
-    cy.get('#CouponModel_Code').type('ahmed1234');
+    cy.get('#CouponModel_Code').type('ahmed12345');
     cy.get('.mb-5 > .select2 > .selection > .select2-selection > .select2-selection__rendered > .select2-search > .select2-search__field').should('be.visible').click();
     cy.get('[role="group"] > .select2-results__options > .select2-results__option').should('be.visible').click();
     cy.get('.row > :nth-child(1) > .btn').click();
@@ -341,3 +301,8 @@ it('No input', () => {
 
 
 
+
+
+
+
+      
